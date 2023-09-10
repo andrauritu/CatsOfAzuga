26582b256002cf30959e07848e2459482a5cb559
andraauritu <zuzulaptezuzulapte@yahoo.com>
2023-09-11 00:03:25 +0300

    repaired image preview issue

--- public/javascripts/displayFilesImages.js
+++ public/javascripts/displayFilesImages.js
@@ -1,31 +1,22 @@
-// function previewMultiple(event) {
-//     var images = document.getElementById("image");
-//     var number = images.files.length;
-//     for (i = 0; i < number; i++) {
-//         var urls = URL.createObjectURL(event.target.files[i]);
-//         document.getElementById("formFile").innerHTML += '<img src="' + urls + '">';
-//     }
-// }
+document.getElementById("image").addEventListener("change", previewMultiple);
 
 function previewMultiple(event) {
-    const images = document.getElementById("image");
-    const number = images.files.length;
+    const images = event.target.files;
     const formFileElement = document.getElementById("formFile");
 
     // Clear the existing content of formFileElement
     formFileElement.innerHTML = '';
 
-    for (i = 0; i < number; i++) {
-        const file = event.target.files[i];
+    for (let i = 0; i < images.length; i++) {
+        const file = images[i];
         const urls = URL.createObjectURL(file);
 
         // Create a new div to hold the image and file name
         const imageDiv = document.createElement('div');
         imageDiv.classList.add('image-preview');
         imageDiv.innerHTML = `
-            <img src="${urls}">
-            <p>${file.name.substring(0, 3)}(...)${file.name.substring(file.name.length - 4, file.name.length)}</p>
-        `;
+    <img src="${urls}">
+        <p>${file.name.substring(0, 3)}(...)${file.name.substring(file.name.length - 4, file.name.length)}</p>`;
 
         formFileElement.appendChild(imageDiv);
     }
